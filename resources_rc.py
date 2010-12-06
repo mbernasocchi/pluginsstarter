f71ee8f5d8dd9412405f49b90fc1bebe48c83077
Marco Bernasocchi <marco@bernawebdesign.ch>
2010-12-06 19:07:45 +0100

    Added make package comanf to makefile

--- resources_rc.py
+++ resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Mon Dec 6 18:26:39 2010
+# Created: Mon Dec 6 18:33:26 2010
 #      by: The Resource Compiler for PyQt (Qt v4.7.0)
 #
 # WARNING! All changes made in this file will be lost!
